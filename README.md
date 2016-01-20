5ad71a20554d5cfdb160a8ccb338b1d5138928ba
Rob <robjrenwick@gmail.com>
2016-01-20 14:34:42 +1000

    new commit - first

---
+++ README.md
@@ -0,0 +1,27 @@
+# PSD to HTML Conversion
+
+This repository contains the web development code used to convert a photoshop PSD design comp of a web landing page into a working HTML template page.
+
+This landing page was created to be mobile first responsive, from a single static comp, therefore it is not pixel perfect. This is a natural side effect of producing fluid and adaptive pages for more than just one screen size.
+
+This page was also coded using [Flex Box](http://caniuse.com/#search=flex) CSS and is therefore only compatible with the latest browsers (not IE). I have intentionally not used a fallback.
+
+## Tools / Workflows
+
+The following frameworks/tools were used:
+* HTML / CSS
+* Sass (scss)
+* JavaScript / jQuery:
+* scrollReveal, scrollTo
+* Google font: Lato
+* Font Awesome icons
+* Gulp, Bower, & Git
+
+
+## PSD Design
+
+This landing page was designed by ![Evgenia Kornienko](https://www.behance.net/JaneKorn)
+
+Adobe [Creative Cloud Extract](http://www.adobe.com/creativecloud/extract.html) was used to source the graphical elements and font information.
+
+![PSD Design](http://imgur.com/Hi4AUnP)
---
+++ scripts/main.js
@@ -0,0 +1,68 @@
+
+// Prevent console.log from generating errors in IE for the purposes of the demo
+    // if ( ! window.console ) console = { log: function(){} };
+    // Scrollreveal initialize
+
+
+  // individual options for Scroll Reveal elements
+      var contactReveal = {
+        origin: 'right',
+        distance: '200px',
+        duration: 800
+      },
+      workH4Reveal = {
+        origin: 'left',
+        distance: '100px',
+        duration: 500
+      };
+
+    window.sr = ScrollReveal();
+    sr.reveal('.services');
+    sr.reveal('#what h4');
+    sr.reveal('#work h4', workH4Reveal);
+    sr.reveal('#contact', contactReveal);
+
+
+$(document).ready(function(){
+
+  // Navigation Functionality
+  // -------------------------------------------------
+
+  // define navigation components as variables
+  var $menuIcon = $('.menu-icon'),
+    $navigation = $('.navigation'),
+    $mainNavigation = $('.main-navigation'),
+    $navigationLink = $('.main-navigation a');
+
+  // add active class to clicked menu item to display nav
+    $menuIcon.click(function(e) {
+    e.preventDefault();
+    $navigation.toggleClass('active');
+  });
+
+  // store target sections of index page in array
+    var $targetArea = [
+      $('#hero'),
+      $('#about'),
+      $('#services'),
+      $('#what'),
+      $('#work'),
+      $('#contact')
+    ];
+
+  // click event for nav links, determine child index
+  //        and use corresponding target array location
+    $($navigationLink).click(function(){
+      var $index = $($navigationLink).index(this);
+      $.scrollTo($targetArea[$index], 1800);
+      // hide navigation menu
+      $navigation.removeClass('active');
+      return false;
+    });
+
+
+
+
+
+
+});
